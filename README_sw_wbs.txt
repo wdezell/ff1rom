c26388d4f5929e11fe0d64df94c7acd66d4d8076
William D. (Bill) Ezell <Bill@narus.xyz>
2020-11-19 13:26:49 -0600

    initial repo creation

---
+++ README_sw_wbs.txt
@@ -0,0 +1,55 @@
+    Work-In-Progress Work Breakdown Schedule for Firefly SBC ROM/BIOS
+
+
+    1. Page Zero Skeleton
+
+        Defs for the IRQ handlers and vectors 
+        Boot jump
+        Define general memory layout
+        
+    2. ROM-2-RAM Switcher
+
+        Copy ROM to RAM on boot
+
+    3. Initializations
+
+        General HW defs for board
+        Define & enable Stack
+        Initialize basic board operational params like default serial console baud
+        Boot console splash
+        
+    4. Boot Mode Switch Dispatcher
+
+        Stubs for dispatch targets
+        Read boot switches and transfer execution to mode-appropriate code
+
+    5. Diagnostics Simple I/O Package
+
+        Wrapper for hex LED output?
+        Simple serial output
+
+    6. Diagnostics
+
+        RAM test, output to LEDs
+        RAM test, output to serial console 
+        Serial Port tests
+        RO/RI/RTS/CTS tests
+        Baud & param changes w/ dummy text output
+        Port A R/W
+        Port B R/W
+        PIO Tests
+        Loopback
+        Port A R/W
+        Port B R/W
+
+    7. Mode 2 IRQ I/O Package
+
+    8. Machine Language Monitor
+
+    9. Board Hardware Utilities
+    
+        Card format & copy
+        
+    10. Forth Interpeter
+
+    11. CP/M loader
---
+++ src/ff1_gbr_main.asm
@@ -0,0 +1,128 @@
+    .NOCODES
+    ;;**********************************************************************
+    ;;
+    ;;  PROGRAM:  FF1GBR_MAIN -- GENERAL BOOT ROM FOR THE WDE8016 'FIREFLY' 
+    ;;                           REVISION 1 BOARD
+    ;;           
+    ;;  AUTHOR:   WILLIAM D. EZELL      
+    ;;  DATE:     
+    ;;                                                               
+    ;;  PURPOSE:  PERFORMS BOARD INITIALIZATION, DISPATCH TO LAUNCH HIGHER-LEVEL
+    ;;            OPERATIONS, DIAGNOSTIC UTILITIES, AND CORE ROUTINES FOR USE BY
+    ;;            HIGHER-LEVEL SOFTWARE
+    ;;
+    ;;
+    ;;  ASSEMBLED USING THE TELEMARK ASSEMBLER (TASM) VERSION 3.1 
+    ;;  BY THOMAS N. ANDERSON, SQUAK VALLEY SOFTWARE
+    ;;
+    ;;  USING THE FOLLOWING TASM SETTINGS:
+    ;;    EXPORT TASMOPTS='-80 -A -B -E -F00 -LAL -S -Y'     
+    ;;
+    ;;    -80   Z80 INSTRUCTION SET
+    ;;    -A    ENABLE ALL ASSEMBLY CONTROL CHECKS (WARNINGS)
+    ;;    -B    RAW BINARY OBJECT FILE OUTPUT FORMAT (SUITABLE FOR PROMS)
+    ;;          (-C 'CONTIGUOUS BLOCK' IS IMPLIED)
+    ;;    -E    EXPAND SOURCE MACROS/INCLUDES
+    ;;    -F    FILL UNUSED MEMORY WITH VALUE
+    ;;    -LAL  SHOW ALL LABELS IN LONG FORM
+    ;;    -S    SYMBOL FILE GENERATION
+    ;;    -Y    ENABLE ASSEMBLY TIMING
+    ;;
+    ;;  SOURCE FORMATTING NOTE:
+    ;;    USING SOFT TABS @ 4 SPACES FOR COLUMNAR ALIGNMENT
+    ;;    OPCODE IN COL 9, OPERAND IN COL 17, COMMENTS COL 25 TYP
+    ;;
+    ;; **********************************************************************
+        .CODES
+        .TITLE "FF-ROM -- SYSTEM ROM FOR THE WDE8016 'FIREFLY' BOARD"
+        .LIST
+        .NOPAGE
+                
+
+        ;; *** BOOTSTRAP ***
+        .ORG    ROM
+        JP      RESET
+
+        .ORG    03H
+        
+RSRVD1: .DS     1       ; RESERVED
+IS_RAM: .DB     0       ; USED BY ROM/RAM SWAP TO DETERMINE IF RAM ALREADY ACTIVE (0 = ROM)
+REDIST: .DB     0       ; USED BY PHASE 2 CODE REDISTRIBUTION (0 = NOT REDISTRIBUTED )
+SWSHDW: .DS     1       ; SHADOW COPY OF CFG SWITCH BITS USED BY INIT RETURN
+SPARE1: .DS     1       ; SPARE BYTE
+
+;; -------------------------------------------------------------
+;; ZERO PAGE JUMP VECTORS & INTERRUPT HANDLERS
+;; -------------------------------------------------------------
+        ;; MODE 0 MASKABLE INTERRUPT VECTORS
+        ;; NB - RET INSTEAD OF RETI/RETN SINCE WE'RE NOT GOING TO USE IM1 BUT RATHER CALL AS SUBS
+        
+        .ORG    08H     ; RST08 
+        RET             ; UNUSED
+    
+        .ORG    10H     ; RST10 
+        RET             ; UNUSED
+    
+        .ORG    18H     ; RST18 
+        RET             ; UNUSED
+    
+        .ORG    20H     ; RST20 
+        RET             ; UNUSED
+    
+        .ORG    28H     ; RST28 
+        RET             ; UNUSED
+    
+        .ORG    30H     ; RST30 
+        RET             ; UNUSED
+        
+
+        ;; MODE 1 MASKABLE INTERRUPT HANDLER
+MSKISR: .ORG    38H
+        EI
+        RETI            ; UNUSED
+
+        
+    ;; NON-MASKABLE INTERRUPT HANDLER
+NMIISR: .ORG    66H        
+        RETN            ; UNUSED
+        
+                
+        ;; MODE 2 PROGRAMMABLE INTERRUPT VECTORS
+INTVEC: .ORG    $ & 0FFFF0H | 10H
+
+        ;; WIP
+
+        
+        .ORG    100H
+;; -------------------------------------------------------------
+;; MAIN START
+;; -------------------------------------------------------------
+RESET:  .EQU    $
+        DI                  ; NO INTERRUPTS UNTIL WE WANT THEM
+        LD      SP, STACK   ; INIT STACK POINTER SO WE CAN CALL SUBS
+        
+        
+    
+        ;; WIP CALL SWITCH READ & GET MODES
+
+
+;; -------------------------------------------------------------
+;; SUPPORT MODULES
+;; -------------------------------------------------------------
+#INCLUDE "hwdefs.asm"       ;; I/O MAP AND CONSTANTS FOR THE REV 1 BOARD
+
+
+
+;; -------------------------------------------------------------
+;; TASM ROM MARKER
+;;
+;; FORCE TASM TO TOUCH A SINGLE BYTE AT THE END OF ROM IN ORDER
+;; IMAGE SIZED EXACTLY FOR THE ROM, ELSE CODE GENERATION WILL 
+;; STOP AFTER THE LAST INSTRUCTION OR DATA BLOCK DEFINITION.
+;; -------------------------------------------------------------
+        .ORG    ROMEND-1
+        .CHK    LOWMEM  ; BYTE VALUE = CHECKSUM FROM ADDRESS 00H THRU PREV BYTE
+        .END
+
+
+
